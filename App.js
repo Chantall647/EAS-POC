d802c2a817cb6a3b465590eca82b535027a7b5f7
ioana.buliga <ioana.buliga@devnest.ro>
2022-11-24 15:01:36 +0200

    app from scratch

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const appVersion = Constants?.manifest?.version;
+
+
+// useEffect(()=>{
+//   const getProductList = async () => {
+//     try{
+//       const jsonValue = await AsyncStorage.getItem('productList')
+//       console.log(jsonValue)
+//       return jsonValue != null ? JSON.parse(jsonValue) : null;
+//     }
+//   }
+//   getProductList()
+// },[selectedProducts])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{appVersion}</Text>
     </View>
   );
 }
