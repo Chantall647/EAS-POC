2436ac01b134b7345971bfb4b5b708baff89e66f
ioana.buliga <ioana.buliga@devnest.ro>
2022-11-24 15:06:33 +0200

    removed leftover code

--- App.js
+++ App.js
@@ -7,18 +7,6 @@ export default function App() {
 
   const appVersion = Constants?.manifest?.version;
 
-
-// useEffect(()=>{
-//   const getProductList = async () => {
-//     try{
-//       const jsonValue = await AsyncStorage.getItem('productList')
-//       console.log(jsonValue)
-//       return jsonValue != null ? JSON.parse(jsonValue) : null;
-//     }
-//   }
-//   getProductList()
-// },[selectedProducts])
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
